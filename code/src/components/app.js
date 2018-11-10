e9ae5a8abc3fded624a2dd176bf00e92f4d77f1f
Caroline Granlund <granlundcaroline@gmail.com>
2018-11-10 21:48:57 +0100

    assignment completed by Caroline

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,5 +1,6 @@
-import React from "react"
-import openGdaxWebsocket from "../gdax-websocket"
+import React from "react";
+import openGdaxWebsocket from "../gdax-websocket";
+import { ResponsiveLine } from '@nivo/line';
 
 class App extends React.Component {
 
@@ -17,18 +18,29 @@ class App extends React.Component {
 
   handleNewTickerMessage = newTickerMessage => {
     this.setState(previousState => ({
-      tickerMessages: previousState.tickerMessages.concat([newTickerMessage])
+      tickerMessages: previousState.tickerMessages.concat([{ ...newTickerMessage, time: new Date() }])
     }))
+    console.log(this.state);
   }
 
   render() {
     return (
       <div>
-        {this.state.tickerMessages.map(msg => (
-          <div key={msg.sequence}>
-            {msg.time}: <strong>{msg.price} EUR</strong>
-          </div>
-        ))}
+        <ResponsiveLine
+          data={[{ id: "bitcoin", data: this.state.tickerMessages.map(data => ({
+            x: data.time.getTime(),
+            y: data.price
+          }))}]}
+          xScale={{
+            type: 'point'
+          }}
+          yScale={{
+            type: 'linear',
+            stacked: true,
+            min: 5600,
+            max: 5650
+          }}
+        />
       </div>
     )
   }
